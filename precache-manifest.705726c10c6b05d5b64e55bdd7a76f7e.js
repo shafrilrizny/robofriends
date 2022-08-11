4dbdfd8517d43929e9cdf52f0dbcfaf87565c7f3
shafrilrizny <riznyapp@gmail.com>
2022-08-11 12:10:43 +0800

    Updates

---
+++ precache-manifest.705726c10c6b05d5b64e55bdd7a76f7e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "db7db5bfbf3f1799bd0d",
+    "url": "/my-app/static/css/main.c23d12f3.chunk.css"
+  },
+  {
+    "revision": "db7db5bfbf3f1799bd0d",
+    "url": "/my-app/static/js/main.db7db5bf.chunk.js"
+  },
+  {
+    "revision": "9863931913954e89bf2e",
+    "url": "/my-app/static/js/1.98639319.chunk.js"
+  },
+  {
+    "revision": "e2cc21b5ccef4e4ca507",
+    "url": "/my-app/static/css/2.3c107068.chunk.css"
+  },
+  {
+    "revision": "e2cc21b5ccef4e4ca507",
+    "url": "/my-app/static/js/2.e2cc21b5.chunk.js"
+  },
+  {
+    "revision": "d283f0a1e45e10d8de1f",
+    "url": "/my-app/static/js/runtime~main.d283f0a1.js"
+  },
+  {
+    "revision": "20f6a8eb0b9b618e54300cd74420429b",
+    "url": "/my-app/static/media/The Quick South St.20f6a8eb.woff"
+  },
+  {
+    "revision": "609da1164b5bc10683e07ab5adae489d",
+    "url": "/my-app/index.html"
+  }
+];
